1778a1ccbf51cc38d2b632e95ab2f1d200ae3529
dpjprogramming <cheesyd64@hotmail.com>
2024-05-02 14:30:55 -0400

    Completed quiz survey summary and all required functions

--- index.php
+++ index.php
@@ -12,12 +12,26 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /survey', function($f3){
+$f3->route('GET|POST /survey', function($f3){
     $choices = getChoices();
     $f3->set('choices', $choices);
 
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $f3->set('SESSION.name', $_POST['name']);
+        $heroes = $_POST['choices'];
+        $f3->set('SESSION.heroes', $heroes);
+
+        $f3->reroute('summary');
+    }
+
     $view = new Template();
     echo $view->render('views/survey.html');
+});
+
+$f3->route('GET /summary', function($f3){
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 
