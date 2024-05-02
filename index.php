b0756720884a90e2d5ba33e9762904ce327945a6
dpjprogramming <cheesyd64@hotmail.com>
2024-05-02 13:32:58 -0400

    added index, home, and survey pages. installed fatfree

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once ('vendor/autoload.php');
+$f3 = Base::instance();
+
+$f3->route('GET /', function(){
+    //echo '<h1> Hello Diner </h1>';
+
+    //render view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
+?>
